5622487efded342cabde04eda0e2f3b639fa2681
Adarsh-Chaurasia <chaurasiadarsh11@gmail.com>
2022-04-17 11:34:14 +0530

    add a title field functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let addBtn = document.getElementById("addBtn");
 
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -13,10 +14,15 @@ addBtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-
-    notesObj.push(addTxt.value);
+    // Array of objects
+    let myObj = {
+        title : addTitle.value,
+        text : addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value= "";
    
     showNotes();
 
@@ -36,8 +42,8 @@ function showNotes() {
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
          <div class="card-body">
         
-          <h5 class="card-title">Note ${index + 1}</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title"> ${element.title}</h5>
+          <p class="card-text">${element.text}</p>
           <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
       </div>
